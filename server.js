1380bfcbb49a7e63d0ceb5a848a19f0cb840db49
Ranim Bouraoui <ranimbouraoui3@gmail.com>
2026-02-20 10:15:30 +0100

    Modification du serveur dans server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // ============================================
 // IMPORTS
 // ============================================
-// Branche B : Application Express
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
